11f49961c5d576f233786a7c74b53eec465842dc
eman <eman>
2017-01-05 16:12:37 +0000

    c

--- left-half/src/main.rs
+++ left-half/src/main.rs
@@ -3,15 +3,15 @@
 
 #[macro_use] extern crate teensy3;
 
-use teensy3::bindings;
+use teensy3::bindings as t;
 use teensy3::serial::Serial;
 
 #[no_mangle]
 pub unsafe extern fn main() {
     // Blink Loop
 
-    bindings::pinMode(13, bindings::OUTPUT as u8);
-    bindings::digitalWrite(13, bindings::LOW as u8);
+    t::pinMode(13, t::OUTPUT as u8);
+    t::digitalWrite(13, t::LOW as u8);
     let mut ser = Serial{};
 
     loop {
@@ -22,24 +22,30 @@ pub unsafe extern fn main() {
         hello(&ser).unwrap();
 
         // Don't spam the console
-        bindings::delay(1000);
+        t::delay(1000);
     }
 }
 
 /// Blink the light twice to know we're alive
-pub unsafe fn alive() {
+unsafe fn alive() {
     for _ in 0..2 {
-        bindings::digitalWrite(13, bindings::LOW as u8);
-        bindings::delay(200);
-        bindings::digitalWrite(13, bindings::HIGH as u8);
-        bindings::delay(200);
-        bindings::digitalWrite(13, bindings::LOW as u8);
-        bindings::delay(200);
+        t::digitalWrite(13, t::LOW as u8);
+        t::delay(200);
+        t::digitalWrite(13, t::HIGH as u8);
+        t::delay(200);
+        t::digitalWrite(13, t::LOW as u8);
+        t::delay(200);
     }
 }
 
 /// Send a message over the USB Serial port
-pub fn hello(ser: &Serial) -> Result<(),()> {
+fn hello(ser: &Serial) -> Result<(),()> {
     let msg = "Hello Teensy Rusty World!\n\r";
     ser.write_bytes(msg.as_bytes())
 }
+
+
+fn setup_wire() {
+    t::
+}
+
--- right-half/src/main.cpp
+++ right-half/src/main.cpp
@@ -3,6 +3,16 @@
 
 byte val = 0;
 
+static int ROW_PINS[] = {
+  1, 2, 3
+};
+#define ROW_PINS_LEN (sizeof(row_pins) / sizeof(row_pins[0]))
+
+static int COL_PINS[] = {
+  1, 2, 3
+};
+#define COL_PINS_LEN (sizeof(col_pins) / sizeof(col_pins[0]))
+
 void setup()
 {
   pinMode(RED_LED, OUTPUT);
@@ -25,12 +35,38 @@ void loop()
 
 void setup_real() {
   Wire.begin();
+
+  // initialize pins
+  for (int i = 0; i < COL_PINS_LEN; i++) {
+    int col_pin = COL_PINS[i];
+    pinMode(col_pin, INPUT);
+    digitalWrite(col_pin, HIGH); // pull-up TODO: make sure this works
+  }
+  for (int i = 0; i < ROW_PINS_LEN; i++) {
+    int row_pin = ROW_PINS[i];
+    pinMode(row_pin, OUTPUT);
+    digitalWrite(row_pin, HIGH);
+  }
 }
 
 void loop_real() {
+  unsigned long pressed_keys = 0; // u32
+  int key_idx = 0;
+  for (int row_idx = 0; row_idx < ROW_PINS_LEN; row_idx++) {
+    int row_pin = ROW_PINS[row_idx];
+    digitalWrite(row_pin, LOW);
+    for (int col_idx = 0; col_idx < COL_PINS_LEN; col_idx++) {
+      int col_pin = COL_PINS[col_idx];
+      pressed_keys |= (unsigned long)(!digitalRead(col_pin) & 1) << key_idx;
+      key_idx++;
+    }
+    digitalWrite(row_pin, HIGH);
+  }
+
   Wire.beginTransmission(44);
-  Wire.write(val);
+  Wire.write(&pressed_keys, 4);
+  //Wire.write(val);
+  //val += 1;
   Wire.endTransmission();
-  val += 1;
   delay(500);
 }

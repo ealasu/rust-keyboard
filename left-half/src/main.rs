0178e423737935a888983fc2f4bf56294e04b7ac
eman <eman>
2017-02-12 11:38:08 -0700

    switch to mchck

--- left-half/src/main.rs
+++ left-half/src/main.rs
@@ -1,76 +1,201 @@
+#![feature(lang_items,core_intrinsics,asm)]
 #![no_std]
 #![no_main]
+#![crate_type="staticlib"]
 
-#[macro_use] extern crate teensy3;
+use core::intrinsics::volatile_store;
 
-mod circular_buffer;
-
-use circular_buffer::CircularBuffer;
-use teensy3::bindings as t;
-use teensy3::serial::Serial;
-
-static mut RIGHT_KEYS: CircularBuffer<u8> = CircularBuffer {
-    data: [0; 4],
-    write_pos: 0,
-};
-
-//static ser: Serial = Serial {};
-
-const LED_PIN: u8 = 13;
-
+#[lang="eh_personality"]
+extern "C" fn eh_personality() {}
+#[lang="panic_fmt"]
 #[no_mangle]
-pub unsafe extern fn main() {
-    t::pinMode(LED_PIN, t::OUTPUT as u8);
-    t::digitalWrite(LED_PIN, t::LOW as u8);
-    alive();
-
-    t::Wire.begin2(44);
-    t::Wire.onReceive(Some(on_receive));
-
-    // Blink Loop
+pub extern "C" fn rust_begin_unwind(_fmt: &core::fmt::Arguments,
+                                    _file_line: &(&'static str, usize))
+                                    -> ! {
+    loop {}
+}
 
 
-    //let ser = Serial{};
-    //ser.begin(9600);
+macro_rules! WDOG_UNLOCK  {() => (0x4005200E as *mut u16);} // Watchdog Unlock register
+macro_rules! WDOG_STCTRLH {() => (0x40052000 as *mut u16);} // Watchdog Status and Control Register High
+macro_rules! GPIO_CONFIG  {() => (0x40048038 as *mut u32);}
+macro_rules! PORTB_PCR16   {() => (0x4004A040 as *mut u32);}
+macro_rules! GPIOB_PDDR   {() => (0x400FF054 as *mut u32);}
+macro_rules! GPIOB_PDOR   {() => (0x400FF040 as *mut u32);}
+
+extern "C" {
+    static mut _sflashdata: u32;
+    static mut _sdata: u32;
+    static mut _edata: u32;
+    static mut _sbss: u32;
+    static mut _ebss: u32;
+    fn _estack();
+}
+
+#[link_section=".vectors"]
+#[allow(non_upper_case_globals)]
+#[no_mangle]
+pub static ISRVectors: [Option<unsafe extern "C" fn()>; 62] = [
+    Some(_estack), // Stack pointer
+    Some(startup), // Reset
+    Some(isr_nmi), // NMI
+    Some(isr_hardfault), // Hard Fault
+    Some(isr_mmfault), /* CM3 Memory Management Fault */
+    Some(isr_busfault), /* CM3 Bus Fault */
+    Some(isr_usagefault), /* CM3 Usage Fault */
+    Some(isr_reserved_1), /* Reserved - Used as NXP Checksum */
+    None, // Reserved
+    None, // Reserved
+    None, // Reserved
+    Some(isr_svcall), // SVCall
+    Some(isr_debugmon), /* Reserved for debug */
+    None, // Reserved
+    Some(isr_pendsv), // PendSV
+    Some(isr_systick), /* SysTick */
+
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+    Some(default_handler),
+];
+
+const LED_PIN: u32 = 1 << 16;
+
+pub unsafe extern "C" fn startup() {
+    let mut src: *mut u32 = &mut _sflashdata;
+    let mut dest: *mut u32 = &mut _sdata;
+
+    volatile_store(WDOG_UNLOCK!(), 0xC520);
+    volatile_store(WDOG_UNLOCK!(), 0xD928);
+    volatile_store(WDOG_STCTRLH!(), 0x01D2);
+
+    while dest < &mut _edata as *mut u32 {
+        *dest = *src;
+        dest = ((dest as u32) + 4) as *mut u32;
+        src = ((src as u32) + 4) as *mut u32;
+    }
+
+    dest = &mut _sbss as *mut u32;
+
+    while dest < &mut _edata as *mut u32 {
+        *dest = 0;
+        dest = ((dest as u32) + 4) as *mut u32;
+    }
+
+    // Enable system clock on all GPIO ports - page 254
+    *GPIO_CONFIG!() = 0x00043F82; // 0b1000011111110000010
+    // Configure the led pin
+    //*PORTC_PCR5!() = 0x00000143; // Enables GPIO | DSE | PULL_ENABLE | PULL_SELECT - page 227
+    *PORTB_PCR16!() = 0x00000143; // Enables GPIO | DSE | PULL_ENABLE | PULL_SELECT - page 227
+    // Set the led pin to output
+    *GPIOB_PDDR!() = LED_PIN; // pin 5 on port c
+
+    rust_loop();
+}
+
+pub fn led_on() {
+    unsafe {
+        volatile_store(GPIOB_PDOR!(), LED_PIN);
+    }
+}
+
+pub fn led_off() {
+    unsafe {
+        volatile_store(GPIOB_PDOR!(), 0x0);
+    }
+}
+
+pub fn delay(ms: i32) {
+    for _ in 0..ms * 2250 {
+        unsafe {
+            asm!("NOP");
+        }
+    }
+}
+
+pub fn rust_loop() {
     loop {
-        // Show we are alive
-        //alive();
-
-        // If the serial write fails, we will halt (no more alive blinks)
-        //hello().unwrap();
-
-        // Don't spam the console
-        t::delay(1000);
+        led_on();
+        delay(1000);
+        led_off();
+        delay(1000);
     }
 }
 
-/// Blink the light twice to know we're alive
-unsafe fn alive() {
-    for _ in 0..2 {
-        t::digitalWrite(LED_PIN, t::LOW as u8);
-        t::delay(200);
-        t::digitalWrite(LED_PIN, t::HIGH as u8);
-        t::delay(200);
-        t::digitalWrite(LED_PIN, t::LOW as u8);
-        t::delay(200);
-    }
+
+pub unsafe extern "C" fn default_handler() {
+    loop {}
 }
 
-/// Send a message over the USB Serial port
-//fn hello() -> Result<(),()> {
-    //let msg = "Hello Teensy Rusty World!\n\r";
-    //ser.write_bytes(msg.as_bytes())
-//}
-
-//fn got_data() -> Result<(),()> {
-    //let msg = "Got data!\n\r";
-    //ser.write_bytes(msg.as_bytes())
-//}
-
-pub unsafe extern fn on_receive(c: i32) {
-    alive();
-    //got_data().unwrap();
-    for _ in 0..c {
-        RIGHT_KEYS.push(t::Wire.read() as u8);
-    }
+pub unsafe extern "C" fn isr_nmi() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_hardfault() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_mmfault() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_busfault() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_usagefault() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_reserved_1() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_svcall() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_debugmon() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_pendsv() {
+    loop {}
+}
+pub unsafe extern "C" fn isr_systick() {
+    loop {}
 }

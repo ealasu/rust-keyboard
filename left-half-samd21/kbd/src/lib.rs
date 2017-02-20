a8cc6b42314ff5037c3102290921d975a5adcd43
eman <eman>
2017-02-19 19:33:14 -0700

    c

--- left-half-samd21/kbd/src/lib.rs
+++ left-half-samd21/kbd/src/lib.rs
@@ -16,23 +16,25 @@ mod macros;
 mod lang_items;
 mod wiring;
 
-
 use cortex_m::asm::nop;
+use wiring::PinMode::*;
+use wiring::PinState::*;
 
+const LED: u32 = 26;
 
 #[no_mangle]
 pub extern fn kbd_run_loop() {
-    wiring::pin_mode(26, wiring::PinMode::Output);
+    wiring::pin_mode(LED, Output);
     loop {
-        wiring::digital_write(26, wiring::PinState::High);
+        wiring::digital_write(LED, High);
         delay_with_nop();
-        wiring::digital_write(26, wiring::PinState::Low);
+        wiring::digital_write(LED, Low);
         delay_with_nop();
     }
 }
 
 fn delay_with_nop() {
-    for _ in 0..1000000{
+    for _ in 0..1000000 {
         nop();
     }
 }
--- left-half-samd21/src/main.ino
+++ left-half-samd21/src/main.ino
@@ -1,11 +1,11 @@
 #include "kbd.h"
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(200);
+  //pinMode(LED_BUILTIN, OUTPUT);
+  //digitalWrite(LED_BUILTIN, HIGH);
+  //delay(200);
+  //digitalWrite(LED_BUILTIN, LOW);
+  //delay(200);
 }
 
 void loop() {

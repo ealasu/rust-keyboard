8385a976b74eaf73349ecb45f720434f621c7c15
eman <eman>
2017-04-07 22:14:06 -0700

    works

--- kbd/src/framed/frame_sink.rs
+++ kbd/src/framed/frame_sink.rs
@@ -3,12 +3,26 @@ use futures::{Poll, Async, AsyncSink, StartSend};
 use futures::sink::Sink;
 use super::{SOF, ESC, ESC_SOF, ESC_ESC, CRC};
 
+macro_rules! try_send {
+    ($e:expr) => (match $e {
+        Ok(::futures::AsyncSink::Ready) => {},
+        Ok(::futures::AsyncSink::NotReady(_)) => return Ok(::futures::Async::NotReady),
+        Err(e) => return Err(From::from(e)),
+    })
+}
+
+enum State {
+    Empty,
+    Data { pos: usize },
+    AfterEsc { escaped: u8, pos: usize },
+}
+
 pub struct FrameSink<'a, Inner, Item, F> {
     inner: Inner,
     _item: PhantomData<Item>,
     encoder: F,
     buf: &'a mut [u8],
-    pos: Option<usize>,
+    state: State,
 }
 
 impl<'a, Inner, Item, F> FrameSink<'a, Inner, Item, F>
@@ -19,7 +33,7 @@ where Inner: Sink<SinkItem=u8>, F: FnMut(Item, &mut [u8]) {
             _item: PhantomData,
             encoder: encoder,
             buf: buf,
-            pos: None,
+            state: State::Empty,
         }
     }
 }
@@ -30,41 +44,57 @@ where Inner: Sink<SinkItem=u8>, F: FnMut(Item, &mut [u8]) {
     type SinkError = Inner::SinkError;
 
     fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
-        if self.pos.is_none() {
-            self.buf[0] = SOF;
-            let buf_len = self.buf.len();
-            let crc = {
-                let payload = &mut self.buf[1..buf_len - 1];
-                (self.encoder)(item, payload);
-                CRC.calc_buf(payload)
-            };
-            self.buf[buf_len - 1] = crc;
-            self.pos = Some(0);
-            //TODO: needed?
-            //self.poll_complete()?;
-            Ok(AsyncSink::Ready)
-        } else {
-            Ok(AsyncSink::NotReady(item))
+        match self.state {
+            State::Empty => {
+                self.buf[0] = SOF;
+                let buf_len = self.buf.len();
+                let crc = {
+                    let payload = &mut self.buf[1..buf_len - 1];
+                    (self.encoder)(item, payload);
+                    CRC.calc_buf(payload)
+                };
+                self.buf[buf_len - 1] = crc;
+                self.state = State::Data { pos: 0 };
+                //TODO: needed?
+                //self.poll_complete()?;
+                Ok(AsyncSink::Ready)
+            }
+            _ => Ok(AsyncSink::NotReady(item))
         }
     }
 
     fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
         loop {
-            if let Some(pos) = self.pos {
-                self.inner.poll_complete()?;
-                if self.inner.start_send(self.buf[pos])? == AsyncSink::Ready {
-                    let pos = pos + 1;
-                    if pos == self.buf.len() {
-                        self.pos = None;
-                        return Ok(Async::Ready(()));
-                    } else {
-                        self.pos = Some(pos);
-                    }
-                } else {
-                    return Ok(Async::NotReady)
+            match self.state {
+                State::AfterEsc { pos, escaped } => {
+                    try_send!(self.inner.start_send(escaped));
+                    self.state = State::Data { pos: pos + 1 };
                 }
-            } else {
-                return self.inner.poll_complete();
+                State::Data { pos } => {
+                    if pos == self.buf.len() {
+                        self.state = State::Empty;
+                        return Ok(Async::Ready(()));
+                    }
+                    self.inner.poll_complete()?;
+                    let v = self.buf[pos];
+                    if pos > 0 && (v == SOF || v == ESC) {
+                        try_send!(self.inner.start_send(ESC));
+                        self.state = State::AfterEsc {
+                            pos: pos,
+                            escaped: match v {
+                                SOF => ESC_SOF,
+                                ESC => ESC_ESC,
+                                _ => unreachable!()
+                            }
+                        };
+                    } else {
+                        try_send!(self.inner.start_send(v));
+                        self.state = State::Data { pos: pos + 1 };
+                    }
+                }
+                State::Empty => {
+                    return self.inner.poll_complete()
+                }
             }
         }
     }

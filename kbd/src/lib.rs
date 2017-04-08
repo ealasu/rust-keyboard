6f0280ec0528f4758c2ba948cf770908d1d4f666
eman <eman>
2017-04-08 10:41:23 -0700

    clean up

--- kbd/src/lib.rs
+++ kbd/src/lib.rs
@@ -1,12 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(feature = "std")] extern crate core;
-#[macro_use] extern crate futures;
-extern crate crc8;
 extern crate wiring;
 
 pub mod msg;
-pub mod msg_reader;
 pub mod matrix;
 pub mod decoder;
 pub mod keycode;
--- kbd/src/msg.rs
+++ kbd/src/msg.rs
@@ -1,64 +1,38 @@
-use crc8;
-
-static CRC: &'static crc8::Crc8 = &crc8::predefined::MAXIM;
-const MAGIC: u8 = 0b10101010;
-
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Msg(pub u32);
 
 impl Msg {
-    pub fn read(buf: &[u8; 5]) -> Option<Msg> {
-        if buf[0] != MAGIC {
-            return None;
-        }
-        if buf[4] != CRC.calc_buf(&buf[0..4]) {
-            return None;
-        }
-        Some(Msg(
-                  buf[1] as u32 |
-                ((buf[2] as u32) << 8) |
-                ((buf[3] as u32) << 16)
-        ))
+    pub fn read(buf: &[u8; 3]) -> Msg {
+        Msg(
+            buf[0] as u32 |
+            ((buf[1] as u32) << 8) |
+            ((buf[2] as u32) << 16)
+        )
     }
 
-    pub fn write(&self) -> [u8; 5] {
-        let bytes = [
-            MAGIC,
+    pub fn write(&self) -> [u8; 3] {
+        [
             (self.0 & 0xFF) as u8,
             ((self.0 >> 8) & 0xFF) as u8,
             ((self.0 >> 16) & 0xFF) as u8,
-        ];
-        [
-            bytes[0],
-            bytes[1],
-            bytes[2],
-            bytes[3],
-            CRC.calc_buf(&bytes),
         ]
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn run_test(bits: u32, expected_bytes: [u8; 5]) {
+    fn run_test(bits: u32, expected_bytes: [u8; 3]) {
         let msg = Msg(bits);
         let buf = msg.write();
         assert_eq!(&buf[..], &expected_bytes[..]);
-        assert_eq!(Msg::read(&buf), Some(msg));
+        assert_eq!(Msg::read(&buf), msg);
     }
 
     #[test]
     fn test_good() {
-        run_test(0x123456, [0b10101010, 0x56, 0x34, 0x12, 47]);
-        run_test(0, [0b10101010, 0, 0, 0, 178]);
-    }
-
-    #[test]
-    fn test_bad() {
-        assert_eq!(Msg::read(&[0,0,0,0,0]), None);
-        assert_eq!(Msg::read(&[0b10101010,0,0,0,0]), None);
+        run_test(0x123456, [0x56, 0x34, 0x12]);
+        run_test(0, [0, 0, 0]);
     }
 }

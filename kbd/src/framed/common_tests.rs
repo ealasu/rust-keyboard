0450a56efd298c33b2efe3a86a8a348d3996270e
eman <eman>
2017-04-07 22:18:20 -0700

    moar tezt

--- kbd/src/framed/common_tests.rs
+++ kbd/src/framed/common_tests.rs
@@ -31,10 +31,26 @@ fn sof_in_payload() {
 }
 
 #[test]
+fn all_sof() {
+    run_test(
+        &[&[SOF,SOF,SOF]],
+        &[SOF,ESC,ESC_SOF,ESC,ESC_SOF,ESC,ESC_SOF,150]
+    );
+}
+
+#[test]
 fn esc_in_payload() {
     run_test(
         &[&[1,ESC,2]],
         &[SOF,1,ESC,ESC_ESC,2,40]
+    );
+}
+
+#[test]
+fn all_esc() {
+    run_test(
+        &[&[ESC,ESC,ESC]],
+        &[SOF,ESC,ESC_ESC,ESC,ESC_ESC,ESC,ESC_ESC,197]
     );
 }
 

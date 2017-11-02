d4ff1c4c4ff22c55e9fd1a5ed40af41f64dd3804
eman <e@m.an>
2017-11-01 21:13:54 -0700

    docs

--- framed/src/constants.rs
+++ framed/src/constants.rs
@@ -1,8 +1,12 @@
 use crc8::{self, Crc8};
 
+/// Start-Of-Frame
 pub const SOF: u8 = 0b01111110;
+/// Escape
 pub const ESC: u8 = 0b01111101;
+/// Escaped Start-Of-Frame
 pub const ESC_SOF: u8 = 0b01011110;
+/// Escaped escape
 pub const ESC_ESC: u8 = 0b01011101;
 
 pub static CRC: &Crc8 = &crc8::predefined::MAXIM;
